a593ee55456b8fe5daf9d7a3a8d48fc6a00e71b0
Zeynel Acikgöz <137368801+zeynelacikgoez@users.noreply.github.com>
2024-12-11 04:25:12 +0100

    Create materials.py

---
+++ materials.py
@@ -0,0 +1,17 @@
+# materials.py
+
+material_properties = {
+    'air': {
+        'absorption': 0.01,
+        'freq': 0.1
+    },
+    'wood': {
+        'absorption': 0.05,
+        'freq': 0.8
+    },
+    'metal': {
+        'absorption': 0.1,
+        'freq': 0.6
+    }
+    # Weitere Materialien können hier hinzugefügt werden
+}
